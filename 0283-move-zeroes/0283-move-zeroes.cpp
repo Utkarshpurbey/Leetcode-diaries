329de1b23db15ec9c60ba64ac83c7b922fa93550
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2025-02-09 12:59:34 +0530

    Time: 0 ms (100%), Space: 23.7 MB (78.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        for(int i =0;i<n;i++){
-            if(nums[i]== 0){
-                int nextNonZero = i;
-                for(int j = i+1;j<n;j++){
-                    if(nums[j]!= 0){
-                        nextNonZero = j;
-                        break;
-                    }
-                }
-                swap(nums[i],nums[nextNonZero]);
+    void moveZeroes(vector<int>& arr) {
+       int noOfNonZero =0;
+       for(int i = 0 ;i<arr.size();i++){
+            if(arr[i]!=0){
+                int temp = arr[i];
+                arr[i] =arr[noOfNonZero];
+                arr[noOfNonZero] = temp;
+                noOfNonZero++;
             }
-        }
-        
+       }
+
     }
 };
