f152178d66a3465fd0eac9668cb2b1a074686276
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2025-03-05 08:06:21 +0530

    Time: 0 ms (100%), Space: 26.1 MB (68.17%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,31 @@
 class Solution {
 public:
+    bool isEven(int x) {return x%2==0;}
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        for(int i :nums){
-            ans^=i;
-        }
-        return ans;
+       int n = nums.size(); 
+       int low = 0, high = n-1;
+       if(n==1) return nums[0];
+
+       while(low<=high){
+        int mid = low + (high-low)/2;
+        int prev = (mid + n -1)%n;
+        int next = (mid +1)%n;
+        if(nums[mid]!=nums[prev] && nums[mid]!=nums[next]) return nums[mid];
+            if(isEven(mid)){
+                // Even index
+                if(nums[mid]==nums[prev]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(nums[mid]== nums[next]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }
+       }
+       return 0;
     }
 };
