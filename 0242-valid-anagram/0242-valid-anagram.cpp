aca0bf8a0f4f2708c52ec48b2e57eeb8bb69f089
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2025-04-13 18:22:37 +0530

    Time: 7 ms (22.14%), Space: 10 MB (12.1%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>m;
+        if(s.size()!=t.size()) return false;
+        int n = s.size();
+        for(int i = 0;i<n;i++){
+            m[s[i]]++;
+        }
+        for(int i = 0;i<n;i++){
+            if(m.find(t[i])!=m.end()){
+                m[t[i]]--;
+            }else{
+                return false;
+            }
+        }
+
+        for(auto i :m){
+            if(i.second!=0) return false;
+        }
+        return true;
+    }
+};
