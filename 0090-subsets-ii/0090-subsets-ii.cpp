a9121f82792522c224545d0e9280b4e86012e3c7
Utkarsh Purbey <77148512+Utkarshpurbey@users.noreply.github.com>
2025-06-08 09:26:28 +0530

    Time: 3 ms (38.74%), Space: 17.7 MB (5.46%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-  void helper(vector<int>arr,int i,int n,vector<int>temp,vector<vector<int>>&ans){
-        if(i == n){
+void helper(int i , vector<int>arr,vector<int>temp,vector<vector<int>>&ans){
+        if(i >=arr.size()){
             ans.push_back(temp);
             return;
         }
+        
+        // include
         temp.push_back(arr[i]);
-        helper(arr,i+1,n,temp,ans);
+        helper(i+1,arr,temp,ans);
         temp.pop_back();
-        while(i+1<arr.size() && arr[i]==arr[i+1]) i++;
-        helper(arr,i+1,n,temp,ans);
+        while(i+1<arr.size() && arr[i] == arr[i+1]) i++;
+        helper(i+1,arr,temp,ans);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>>ans;
-        int n = nums.size();
+         vector<vector<int>>ans;
+        // helper
         sort(nums.begin(),nums.end());
-        helper(nums,0,n,{},ans);
+        helper(0,nums,{},ans);
         return ans;
     }
 };
